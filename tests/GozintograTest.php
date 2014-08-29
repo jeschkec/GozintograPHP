09e4f38e6decaf20df6661f868c0381a7d59a904
Christoph Jeschke <christoph.jeschke@jhp-consulting.eu>
2014-08-29 02:06:00 +0200

    [GOZ-15] phpunit and some basic tests added

---
+++ tests/GozintograTest.php
@@ -0,0 +1,27 @@
+<?php
+//  expand include path to lib/ directory
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . dirname(__FILE__) . PATH_SEPARATOR .  '../');
+
+/**
+ *  GozintograPHP is the main library and does all the work
+ */
+require_once('lib/Gozintogra.php');
+
+class GozintograTest extends PHPUnit_Framework_TestCase
+{
+    public function testStripSingleQuotes()
+    {
+        $unstripped = "'stripped'";
+        $stripped   = 'stripped';
+
+        $this->assertEquals($stripped, GozintograPHP::stripQuotes($unstripped));
+    }
+
+    public function testStripDoubleQuotes()
+    {
+        $unstripped = '"stripped"';
+        $stripped   = 'stripped';
+
+        $this->assertEquals($stripped, GozintograPHP::stripQuotes($unstripped));
+    }
+}
